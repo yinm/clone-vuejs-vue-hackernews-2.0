47dea59d9604ae0db862e645edb446e12373f1f5
yinm <yinm.cminor9@gmail.com>
2018-07-14 14:15:13 +0900

    iAdd createRenderer

---
+++ server.js
@@ -0,0 +1,31 @@
+const fs = require('fs')
+const path = require('path')
+const LRU = require('lru-cache')
+const express = require('express')
+const favicon = require('serve-favicon')
+const compression = require('compression')
+const microcache = require('route-cache')
+const resolve = file => path.resolve(__dirname, file)
+const { createBundleRenderer } = require('vue-server-renderer')
+
+const isProd = process.env.NODE_ENV === 'production'
+const useMicroCache = process.env.MICRO_CACHE !== 'false'
+const serverInfo =
+  `express/${require('express/package.json').version} ` +
+  `vue-server-renderer/${require('vue-server-renderer/package.json').version}`
+
+const app = express()
+
+function createRenderer(bundle, options) {
+  return createBundleRenderer(bundle, Object.assign(options, {
+    // for component caching
+    cache: LRU({
+      max: 1000,
+      maxAge: 1000 * 60 * 15
+    }),
+    // this is only needed when vue-server-renderer is npm-linked
+    basedir: resolve('./dist'),
+    // recommended for performance
+    runInNewContext: false
+  }))
+}
